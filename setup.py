34e742d8c4ec0fe8e52f1aa62aaadfb287dc4736
Matthew Honnibal <honnibal+gh@gmail.com>
2017-10-12 18:04:38 +0200

    Ignore more stuff

--- setup.py
+++ setup.py
@@ -89,7 +89,8 @@ class ExtensionBuilder(distutils.command.build_ext.build_ext):
 
     def get_c_sources(self, start_dir):
         c_sources = []
-        excludes = ['old', 'attic', 'packv', 'scalv', 'cblas', 'other']
+        excludes = ['old', 'attic', 'broken', 'tmp', 'test', 'packv',
+                    'scalv', 'cblas', 'other']
         for path, subdirs, files in os.walk(start_dir):
             for exc in excludes:
                 if exc in path:
